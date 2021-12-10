6f068300c41a2540a4a4e269f279701fc3cc4847
Rutumbhara Kale <84185776+Rutumbhara@users.noreply.github.com>
2021-12-10 22:13:24 +0530

    Add files via upload
    Initial commit.

---
+++ Jarvis.py
@@ -0,0 +1,86 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import webbrowser
+import os
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Ma'am. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 0.5
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")  
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    if 1:
+        query = takeCommand().lower()
+
+        if 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Ma'am, the time is {strTime}")
+
+        elif 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 2)
+            print(results)
+            speak(results)
+
+        elif 'open code' in query:
+            codePath1 = "C:\\Users\\Lenovo\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath1)
+
+        elif 'open python' in query:
+            codePath2 = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\PyCharm Community Edition 2021.3.lnk"
+            os.startfile(codePath2)
+        
+        elif 'open command' in query:
+            codePath3 = "C:\WINDOWS\system32\cmd.exe"
+            os.startfile(codePath3)
+
+        elif 'open run' in query:
+            codePath3 = "C:\Users\Lenovo\Desktop\Run.lnk"
+            os.startfile(codePath3)
